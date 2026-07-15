1450a2fe596370c613a3fbb8f5801c6bb73979d7
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-15 11:04:16 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,45 @@
+#python have built in datatype :-
+#int       -Whole numbers(e.g,1,-10)
+#float     -Decimal numbers(e.g, 2.0,-4.5)
+#boolean   -gives True or False
+#list      -Ordered,Mutuable (e.g., [1,3,5])
+#tuple     -Ordered,Unmutable (e.g., (1,2,3,4))
+#dictionay -Key-Values pairs (e.g., {"name":"raj","class":4}{1:2,3:4})
+#sets      -Unordered collection of unique elements (e.g., {1,2,3,4})
+#string  -Text data enclosed in a quotes(e.g, "hello world","name","Alice")
+
+
+age=45
+print(age)
+print(type(age))
+
+cgpa=8.3
+print(cgpa)
+print(type(cgpa))
+
+name="Raj"
+print(name)
+print(type(name))
+
+
+is_completed=True#can be false
+print(is_completed)
+print(type(is_completed))
+
+
+a={1:2,3:4}
+print(a)
+print(type(a))
+
+b=(1,2)
+print(b)
+print(type(b))
+
+
+c={1,2,3,4}
+print(c)
+print(type(c))
+
+d=[1,"raj"]
+print(d)
+print(type(d))
