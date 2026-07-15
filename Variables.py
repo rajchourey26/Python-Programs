436c9a63f21d2bb25750f8c4e6a7d5098a435858
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-15 10:58:59 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -3,10 +3,11 @@ name="Raj" #dtype-string
 cgpa=8.20#dtype-float
 #age,name,cgpa are variable
 #RULES OF VARIABLE
-1.
-
-
-
+#1.Variable must strat with a letter(a-z,A-Z) or with a underescore (_).
+#Variable can contain numbers,unedrscore.
+#Variable cannot be keyword of python.
+#Varible  cannot have special character(!@#$%&*) unlike undrescore(_).
+#Variables cannot contain spaces (—neither at the beginning, middle, nor end.).
 #EXAMPLES
 age=65#valid
 #a$$ge=56#invalid
