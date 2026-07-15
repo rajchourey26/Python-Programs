bf718da816590a56bb32a8e437a2e216ce7a2b87
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-15 10:48:40 +0530

    Add files via upload

---
+++ Variabeles.py
@@ -0,0 +1,14 @@
+age=56 #datatype-integer
+name="Raj" #dtype-string
+cgpa=8.20#dtype-float
+#age,name,cgpa are variable
+#RULES OF VARIABLE
+1.
+
+
+
+#EXAMPLES
+age=65#valid
+#a$$ge=56#invalid
+__age=50#valid
+a_b_c_=23#valid
