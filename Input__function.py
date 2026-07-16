ff4a2e2553499d475233ce237b00b1bddc3d5f6b
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-16 16:17:22 +0530

    Add files via upload

---
+++ Input__function.py
@@ -0,0 +1,16 @@
+#INPUT FUNCTON = allows user to take input from keyboard hence user can take part in the programs
+
+Name=input("Enter your name:")
+Age=int(input("Enter your age:"))
+# Our output 
+print("My name is",Name ,"and my Age is ",Age)
+
+#Typecasting
+#-It is a way to convert one datatype to other datatype
+#EG 1 : conversion of int to string
+my_int=56
+print(my_int)
+print(type(my_int))
+my_str=str(my_int)
+print(my_str)
+print(type(my_str))
