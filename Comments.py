e7fc9d469add454b169d7f27ac897f30c9e690ff
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-17 17:34:42 +0530

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,17 @@
+#Comments
+#It is to explain the code and it is being ignored by the computer/python.
+
+#1. Single Line comment
+# -It is start with #
+# eg.
+
+print("hey how are you")#this is single line comment
+
+#2. Multipline Line comment
+# -It is start with '''  and end with '''
+# eg.
+
+print("Rohan is my friend") 
+'''this is a
+multiline comment
+'''
