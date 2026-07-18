21e28fe88ac10988cfa6623265d1509fb3bd2d2c
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-18 12:35:31 +0530

    Add files via upload

---
+++ Escape_sequence.py
@@ -0,0 +1,18 @@
+#Escape sequence character in strings 
+#it actually overcame the problem of including the special character in the string
+
+#1.\n -use to jump to newline.
+print("hey how are you\n I am good")
+
+#2.\t - insert a horizontal tab space
+print("hey how are you\tI am good")
+
+#3.\\ - prints a single backslash
+print("C:\\Users:\\Raj") #for the single backslash \ we need to put double \\
+
+#4.\" - print a double quotation mark inside the string
+print("hey how are you \"I am good\"") #for putting the double qoutation " we need to put \"
+
+#5.\' - prints a single quotation mark inside the string
+print("hey how are you\'I am good\'") #for ' --> we have to put \'
+
