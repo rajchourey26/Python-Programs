640651f3e4f51474da1d0f6cc051c420e8084c60
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-22 14:34:54 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#While loop - it is use to iterate over condition which is true
+#They are is useful for when we dont know no. of iteration 
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("\n")                                    #or else we can use the while loop for same
+i=1                                            #we can take this from where we have to start the loop(or from where we want output) ##when I put -1 there it "infinte loop"
+while i<6:#i<=5
+    print(i)
+    i=i+1                                      #these line will continuously(repeat as this is a loop) executed until condition becomes false.
