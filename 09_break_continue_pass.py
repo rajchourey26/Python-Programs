94a0ebe58266da81e15470acacf243e24642db90
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-23 14:20:37 +0530

    Add files via upload

---
+++ 09_break_continue_pass.py
@@ -0,0 +1,10 @@
+for i in range(0,21):
+    #print(i)
+    if i ==3:
+        continue    #1.continue the next itteration from here ,skip this iteration(hence 3 is not printed)
+    print(i)         #2.it says do not execute below continue for this condition 
+    if i==11:
+        break       #it simply stops the next ittration from here  (from this condition)
+    if i==1:
+        pass        #it does nothing is used when syntax require statement but no action required
+
