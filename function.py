ff8450ee1b34ff912c0534c8bca38080d19b761c
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-07-26 23:39:04 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,28 @@
+'''a=2
+b=5
+c=5
+average0=(a+b+c)/3
+print(average0)
+'''
+# making function():
+def average(a,b,c):
+    d=(a+b+c)/3
+    return d
+    #print(d)
+
+#average(1,3,1)
+#average(0,3,0)
+a=average(1,3,1)   # or
+print(a)           # print(average(1,3,1))
+
+a1=average(0,3,0)   # or
+print(a1)           # print(average(0,3,0))
+
+
+
+#a1=3
+#b1=3
+#c1=3
+
+#average1=(a1+b1+c1)/3
+#print(average1)
