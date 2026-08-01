3f53339f706fa9d2049232a70a0a7ef4a3d28a4a
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-08-01 23:59:42 +0530

    Add files via upload

---
+++ Lamda_function.py
@@ -0,0 +1,20 @@
+#lambda--> one line function 
+a=lambda x,y:x+y
+
+print(a(1,2))
+'''
+it is better than 
+def sum(x,y):
+    return x+y
+
+print(sum(1,2))    
+'''
+
+square=lambda x:x*x
+print(square(6))
+'''
+it is better than 
+def square(a):
+    return a*a
+     
+print(square(89)) '''
