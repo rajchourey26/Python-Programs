2f12359dfdc2d4d328109cbe1b01168ed7c4c73c
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-09-14 23:57:13 +0530

    Add files via upload

---
+++ tuple_method.py
@@ -0,0 +1,7 @@
+#tuple ex: a=(3,2,22,13)
+# tuple are faster than list and used as dictionary keys since they are hashable
+t=(3,12,1,54,23,12)
+
+print(t.count(12))#int Return number of occurrences of value.
+
+print(t.index(1))#-> int Return first index of value.
