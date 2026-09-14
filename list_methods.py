762521fc76ec9912890da67cf2947ad835c200e8
Raj Chourey <85687806+rajchourey26@users.noreply.github.com>
2026-09-14 23:41:43 +0530

    Add files via upload

--- list_methods.py
+++ list_methods.py
@@ -1,12 +1,23 @@
 
 print("\nlist method")
+
 list=[2,3,4,5,6,7]
+
 list01=[8,9,10]
-list.append(8) #this will change the original list
-list.pop()#it will remove last element here 8
-list.insert(1,5)#at index 1 the value 5 is inserted
-list.extend(list01)
-list.remove(5)#it will remove element 5
-list.reverse()
-list.sort()
+
+list.append(8) #this will change the original list  [2, 3, 4, 5, 6, 7, 8]
+ 
+list.pop()#it will remove last element here 8  [2, 3, 4, 5, 6, 7]
+
+list.insert(1,5)#at index 1 the value 5 is inserted  [2, 5, 3, 4, 5, 6, 7]
+
+list.extend(list01)#[2, 5, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list.remove(5)#it will remove element 5  [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list.reverse()#it will reverse the whole the list  [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+list.sort()#it sort by default ascending  [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 print(list)
